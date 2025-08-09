2e1941d893af1a13b095679370e1ccc5865a10af
OctavianJH <octavianjhumphreys@icloud.com>
2025-08-09 14:09:14 +0100

    Added new function and fixed syntax/logic errors

--- PasswordStrengthChecker/PWStrengthChecker.py
+++ PasswordStrengthChecker/PWStrengthChecker.py
@@ -1,10 +1,16 @@
+#--------------Libraries---------------------
+import random
+import string
+import re 
+
 #------------------ Global Variables ---------------
 password = ''
 
-
-
-
-
+#------------------ Lists --------------------------
+specialChar = [
+    "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",
+    ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"
+]
 
 #------------------ Subfunctions -------------------
 
@@ -22,17 +28,17 @@ def pwLength():
                 answer = input("Y/N")
 
                 if answer.upper() == 'Y':
-                    newPW = userPW("Enter new password or type '1' if you want me to create one for you: ")
+                    newPW = input("Enter new password or type '1' if you want me to create one for you: ")
 
                     if newPW != '1':
-                        print(f"Your new password is {password}.")
+                        print(f"Your new password is {newPW}.")
                         return newPW
 
 
                     else:
                         excess = len(userPW) - 32 #This gets the exceess amount of digits.
                         shortenedPW = newPW[:-excess]
-                        print(f"Your new password is {password}.")
+                        print(f"Your new password is {shortenedPW}.")
                         return shortenedPW
                         break
                 else:
@@ -41,7 +47,52 @@ def pwLength():
             else:
                 print("PW Accepted.")
                 return userPW
+            
+
+def uniquenessOfPW(password): #Used to check for special characters/numbers.
+    if password.isalpha():
+        print("Password must include numbers.")
+        q = input("Enter new password or type '1' if you want me to create one for you: ")
+
+        if q == '1':
+            num = random.randint(1000, 9999)  # 4 digit number
+            newPW = password + str(num)
+            print(f"Your new password is {newPW}")
+            return newPW
+        else:
+            return q
+
+    elif password.isdigit():
+        print("Password must include letters.")
+        q = input("Enter new password or type '1' if you want me to create one for you: ")
+
+        if q == '1':
+            newPW = ''.join(random.sample(string.ascii_letters, 4)) + password
+            print(f"Your new password is {newPW}")
+            return newPW
+        else:
+            return q
+        
+    else:
+        if re.match("^[a-zA-Z0-9]*$", password):
+            print("No special characters detected.")
+            q = input("Enter new password or type '1' if you want me to create one for you: ")
+
+            if q == '1':
+                number = random.randint(0, len(specialChar) - 1)
+                character = specialChar[number]
+
+                # Example: create a new password with some letters + special character + numbers
+                newPW = ''.join(random.sample(string.ascii_letters, 4)) + character + str(random.randint(10,99))
+                
+                print(f"Your new password is {newPW}")
+                return newPW
+            else:
+                return q
+
 
 #------------------ Main Program ---------------------
 
 password = pwLength()
+password = uniquenessOfPW(password)
+print(f"Final password is: {password}")
